630d8b43569f18b2db5c3830a2e7fe2046228074
Kaeneth Dayao <kendayao@gmail.com>
2020-12-03 12:48:40 -0800

    added home image and styled home image

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,7 +4,12 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home">
-      <p>I am the home</p>
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/YzExNTRjZmUt/YzExNTRjZmUt-YTJjN2Q5MTct-w1500._CB414654619_.jpg"
+        />
+      </div>
     </div>
   );
 }
