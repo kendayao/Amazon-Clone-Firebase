23090f6ccafc340659ada5f0f6a7b0b1c6a57c1d
Kaeneth Dayao <kendayao@gmail.com>
2020-12-03 12:18:58 -0800

    completed and sytled Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
-import './App.css';
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
+      <Header />
+      <Home />
     </div>
   );
 }
