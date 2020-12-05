e911ee240e75456f8a42aecdf4070746150b3365
Kaeneth Dayao <kendayao@gmail.com>
2020-12-04 19:11:32 -0800

    addes selector function to render subtotal price

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,12 @@ export const initialState = {
     basket: [],
 };
 
+//Selector
+export const getBasketTotal=(basket)=>(
+    basket?.reduce((accumalator, basketItem)=>accumalator+basketItem.price,0)
+)
+
+
 const reducer = (state, action)=>{
     switch(action.type){
         case 'ADD_TO_BASKET':
